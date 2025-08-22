d8a30b82e030ca0c430e0284e7508a7d9526579f
Nishanth <smnishanth1142001@gmail.com>
2025-08-22 15:45:06 +0200

    validation to login/signup

--- app/login/page.js
+++ app/login/page.js
@@ -2,11 +2,10 @@
 import { useAuth } from '../contexts/authContext'
 import { doSignInWithEmailAndPassword } from '../../auth'
 import { doSendEmailVerification } from '../../auth'
-import { Navigate } from 'react-router-dom'
+import { Navigate, Route , Routes, BrowserRouter } from 'react-router-dom'
 import React, { useState, useRef } from 'react'
-import {Mail, Lock, Eye, EyeOff, Upload, X } from 'lucide-react'
+import { Mail, Lock, Eye, EyeOff, Upload, X } from 'lucide-react'
 import NeonBackground from '../ui_components/primaryBackground/page'
-
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false)
@@ -32,8 +31,16 @@ export default function LoginPage() {
     <div>
       <NeonBackground />
       <div className='v-full flex h-full flex-row-reverse'>
-        {userLoggedIn && <Navigate to={'/dashboard'} replace={true} />}
-
+        <BrowserRouter>
+          <Routes>
+            {userLoggedIn && (
+              <Route
+                path='/login'
+                element={<Navigate to='/dashboard' replace />}
+              />
+            )}
+          </Routes>
+        </BrowserRouter>
         {/* Background Layer */}
         <div className='relative h-screen w-[40%]'>
           <div className='absolute h-full w-full rounded-l-[250px] bg-[url("/agent_mirror.jpg")] mask-l-from-90% bg-cover bg-center opacity-25'></div>
@@ -53,10 +60,7 @@ export default function LoginPage() {
 
         {/* Main Content */}
         <div className='z-10 flex w-[60%] items-center justify-center px-4'>
-          <form
-            className='w-[50%] rounded-2xl border-5 bg-transparent p-8 shadow-xl'
-           
-          >
+          <form className='w-[50%] rounded-2xl border-5 bg-transparent p-8 shadow-xl'     onSubmit={onSubmit}>
             {/* Decorative Header */}
             {/* Login Form */}
             <div className='space-y-6'>
@@ -68,12 +72,12 @@ export default function LoginPage() {
               <div className="block text-sm font-medium text-white">Username</div> */}
               <div className='relative'>
                 <div className='space-y-2'>
-                   <Mail className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                  <Mail className='absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-gray-400' />
                   <input
                     type='text'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className='pl-10 border-b-primary w-full border-2 border-transparent px-4 py-3 text-white placeholder-gray-400 transition-all duration-300 focus:border-b-blue-400 focus:ring-2 focus:ring-transparent focus:outline-none'
+                    className='border-b-primary w-full border-2 border-transparent px-4 py-3 pl-10 text-white placeholder-gray-400 transition-all duration-300 focus:border-b-blue-400 focus:ring-2 focus:ring-transparent focus:outline-none'
                     placeholder='Email'
                   />
                 </div>
@@ -84,12 +88,12 @@ export default function LoginPage() {
               <div className="block text-sm font-medium text-gray-700">Password</div> */}
               <div className='space-y-2'>
                 <div className='relative'>
-                  <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                  <Lock className='absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-gray-400' />
                   <input
                     type={showPassword ? 'text' : 'password'}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className='pl-10 border-b-primary w-full border-2 border-transparent px-4 py-3 text-white placeholder-gray-400 transition-all duration-300 focus:border-b-blue-400 focus:ring-2 focus:ring-transparent focus:outline-none'
+                    className='border-b-primary w-full border-2 border-transparent px-4 py-3 pl-10 text-white placeholder-gray-400 transition-all duration-300 focus:border-b-blue-400 focus:ring-2 focus:ring-transparent focus:outline-none'
                     placeholder='Password'
                   />
                   <button
@@ -117,8 +121,11 @@ export default function LoginPage() {
 
               {/* Login Button */}
               <div className='flex justify-center'>
-                <button  onClick={onSubmit} className='transparent border-primary mt-4 w-1/2 transform rounded-[5px] border-[1px] border-solid p-2 text-center text-[20px] font-bold tracking-[0.5rem] text-white uppercase shadow-[0_0_10px_rgba(59,130,246)] transition-all duration-300 hover:scale-105 hover:cursor-pointer hover:bg-white hover:text-black hover:shadow-[0_0_50px_rgba(59,130,246)]'>
-                  <a href='/register'>Sign In</a>
+                <button
+              
+                  className='transparent border-primary mt-4 w-1/2 transform rounded-[5px] border-[1px] border-solid p-2 text-center text-[20px] font-bold tracking-[0.5rem] text-white uppercase shadow-[0_0_10px_rgba(59,130,246)] transition-all duration-300 hover:scale-105 hover:cursor-pointer hover:bg-white hover:text-black hover:shadow-[0_0_50px_rgba(59,130,246)]'
+                >
+               SIGN IN
                 </button>
               </div>
 
